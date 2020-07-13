614621578bb089a5090c96d64b3216b6d375ce9c
yueningli95 <yueningli95@163.com>
2020-07-12 17:20:37 -0700

    district generation

---
+++ District Generation - Software Engineering/database/add_data.sql
@@ -0,0 +1,12 @@
+USE district_generation;
+
+INSERT INTO RegisteredUser (id, username, firstname, lastname,email, pass_word) VALUES
+  (1, 'Joe','Joe','lin', 'awesomejane@ftw.com','Lin1000001@'),
+  (2, 'Bill','Bill','Gates', 'canontimore@sjfeoja.com','Lafejioi0001@'),
+  (3, 'Marry','Maria','Galberg', 'awsomfemarry@ftw.com','sjafoeiajif@');
+
+
+INSERT INTO Admin (id, first_name, last_name) VALUES
+  (11, 'Joe','lin'),
+   (12, 'Pary','Peng'),
+    (13, 'Amiae','Dam');
